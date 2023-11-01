27ad11607636b93090e600a23dce495bfe7f3e9e
Alexander Pietsch <alexander.pietsch@diva-e.com>
2023-11-01 10:14:56 +0100

    updated for kotlin multiproject

---
+++ src/main/resources/archetype-resources/src/test/kotlin/ExampleTest.kt
@@ -0,0 +1,11 @@
+package ${package}
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ExampleTest {
+    @Test
+    fun testExampleFunction() {
+        assertEquals(4, 2 + 2)
+    }
+}
